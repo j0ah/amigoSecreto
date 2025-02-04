e49462743015be54dfa1168770b54028420d0c1a
j0ah <juanjosechocobar@gmail.com>
2025-02-04 17:30:15 -0300

    crear función agregarAmigo para el botón Añadir

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+
+function agregarAmigo() {
+    let nombresDeAmigos = document.getElementById("amigo").value;
+    let lista = document.getElementById("listaAmigos");
+    let nombreYaEnLista = nombres.find((x) => x === nombresDeAmigos)
+    console.log(nombreYaEnLista);
+    if(nombresDeAmigos === "" || nombresDeAmigos === Number || nombresDeAmigos === " ") {
+        alert("El Nombre no puede estar vacío o contener números.")
+    } else {
+        if(nombresDeAmigos === nombreYaEnLista){
+            alert("El nombre ya está en la lista.");
+            limpiar();
+        } else if (nombresDeAmigos) {
+            nombres.push(nombresDeAmigos);
+            let createLi = document.createElement("li");
+            createLi.innerText = nombresDeAmigos;
+            lista.appendChild(createLi);
+        }
+    }
+    limpiar();
+}
+
+
+
+let limpiar = () => {
+    document.getElementById("amigo").value = "";
+}
